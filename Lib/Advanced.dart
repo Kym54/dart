275a5e25fdb52ba5110ec2e65d3a9ab82e43baa7
Kym54 <samuelfwangui@gmail.com>
2022-11-07 10:26:10 +0300

    Sets and Map

--- Lib/Advanced.dart
+++ Lib/Advanced.dart
@@ -53,12 +53,49 @@ if(isIsFalse || isIsTrue){
   }else print("it is false");
   // ANd logic always gives a false output
 
+//ARRAYS
+  List fruitCollection =['Mango', 'Kiwi', 'Grapes', 'Apples'];
+  var myFruits= [1,2,3,4];
+  print(myFruits[2]);
+  print(fruitCollection[0]);
 
+//Get,Set,Go
 
+main(List <String>arguments){
+  var fruitCollect ={'Mango', 'Grape', 'lime','banana'};
+  print(fruitCollect.lookup('lime'));
+}
+// understanding map() and set()
 
+  var monIntegers= {};
+if(monIntegers.isEmpty){
+  print("the method is a map that has no kwy, value pair.");
+}else print('the method is a set that has no key, value pair');
 
+// reason is because the curl brackets {} are a default for map literals
+  // Map is an object that associates keys and values
+  // sets has keys but they are implicit and are known as indexes
 
+var myProducts={
+  'first':'Seats',
+  'second':'Refrigerator',
+  'third':'Cooking Gar',
+  'forth':'Mobile',
+  'fifth':'Laptop',
+  'sixth':'Router'
+};
+print(myProducts['forth']);
 
+// adding a set type in a map
+  Set mySet={1,2,3,4,5};
+  var mondayProducts={
+    1:'Seat',
+    2:'Refrigerator',
+    3:mySet.lookup(2),
+    4:'Tablet',
+    5:'Laptop',
+  };
+  print(mondayProducts[3]);
 
   var newCar =new Car();
   newCar.carName ="Red Angel";
